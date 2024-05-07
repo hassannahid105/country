25280c4e1d0b6c5483f20d6135faee9475a7e26e
nahidHassan405 <infosharemail405@gmail.com>
2024-05-07 21:34:29 +0600

    handle list function

--- src/Countries/Countries.jsx
+++ src/Countries/Countries.jsx
@@ -5,17 +5,37 @@ import "./Countries.css";
 
 const Countries = () => {
   const [countries, setCountries] = useState([]);
+  const [countryList, SetCountryList] = useState([]);
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
       .then((data) => setCountries(data));
   }, []);
+  const handleCountryList = (country) => {
+    const newCountryList = [...countryList, country];
+    SetCountryList(newCountryList);
+  };
+  console.log(countryList);
   return (
     <div>
       <h3>Country length: {countries.length}</h3>
+      <div>
+        <ul>
+          {countryList.map((countryName, idx) => (
+            <li key={idx}>
+              <img className={"box"} src={countryName.flags.png} alt="" />
+              {countryName.name.common}
+            </li>
+          ))}
+        </ul>
+      </div>
       <div className={"countries-container"}>
         {countries.map((country) => (
-          <Country key={country.cca3} country={country}></Country>
+          <Country
+            key={country.cca3}
+            country={country}
+            handleCountryList={handleCountryList}
+          ></Country>
         ))}
       </div>
     </div>
--- src/Country/Country.jsx
+++ src/Country/Country.jsx
@@ -1,10 +1,13 @@
+import { useState } from "react";
 import "./Country.css";
-const Country = ({ country }) => {
-  console.log(country);
+const Country = ({ country, handleCountryList }) => {
   const { name, flags, area, capital, maps } = country;
-  console.log(maps.googleMaps);
+  const [isVisited, setVisitid] = useState(false);
+  const handleVisited = () => {
+    setVisitid(!isVisited);
+  };
   return (
-    <div className={"country-css"}>
+    <div className={isVisited ? "visit" : "country-css"}>
       <img src={flags.png} alt="" />
       <h3>Country Name: {name.common}</h3>
       <p>Area: {area}</p>
@@ -15,6 +18,14 @@ const Country = ({ country }) => {
           Go to map
         </a>
       </p>
+      <button
+        onClick={() => {
+          handleVisited();
+          handleCountryList(country);
+        }}
+      >
+        Visited
+      </button>
     </div>
   );
 };
